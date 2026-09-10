43c9315a8a2c0928aebc91cee5a99d3d427f134a
Dayaan777 <ahmeddayan18@gmail.com>
2026-09-11 00:43:05 +0500

    Render trust section above footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { DM_Sans, Instrument_Serif, Crimson_Text, Montserrat } from 'next/font/g
 import { CartProvider } from '@/context/cart-context'
 import { WishlistProvider } from '@/context/wishlist-context'
 import { Header } from '@/components/layout/header'
+import { FooterIntro } from '@/components/layout/footer-intro'
 import { Footer } from '@/components/layout/footer'
 import '@/app/globals.css'
 
@@ -81,6 +82,7 @@ export default function RootLayout({
               {children}
             </main>
 
+            <FooterIntro />
             <Footer />
           </WishlistProvider>
         </CartProvider>
